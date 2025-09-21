88f98cf34c627d317c77c4c5bd5e07af1834f3d8
Melany Paola Rivera Lores <38965651+MelsLores@users.noreply.github.com>
2025-09-21 16:41:32 -0600

    config mvn

---
+++ src/main/java/com/example/scidataflow_json_csv_integration/ScidataflowJsonCsvIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.scidataflow_json_csv_integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScidataflowJsonCsvIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScidataflowJsonCsvIntegrationApplication.class, args);
+	}
+
+}
